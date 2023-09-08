dea5b63e4d012c81b9a24bc93875de03b50e9603
mohdanan <mohdanan@cisco.com>
2023-09-08 12:08:25 +0530

    Todo app

---
+++ web1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import functions
+
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+
+
+todos=functions.get_todos()
+
+st.title("My TODO App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+    
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+
+print("Hello")
